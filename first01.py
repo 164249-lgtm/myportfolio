60d01abac4ed57f994f0ef4901aad2697970a275
164249-lgtm <164249@wsk.ac.th>
2026-06-11 02:55:21 +0000

    first01.py

---
+++ first01.py
@@ -0,0 +1,9 @@
+# รับค่าความกว้างและความยาว
+width = float(input("กรอกความกว้าง: "))
+length = float(input("กรอกความยาว: "))
+
+# คำนวณพื้นที่
+area = width * length
+
+print(f"พื้นที่ = {area}")
+Comment
